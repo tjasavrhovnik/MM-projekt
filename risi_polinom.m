29a6a28b92ce4baa467ebca3609fe5273a00031e
tjasavrhovnik <tjasa.vrhovnik@student.fmf.uni-lj.si>
2019-08-18 16:51:18 +0200

    Komentarji Izris tock, ki dolocajo polinom

--- risi_polinom.m
+++ risi_polinom.m
@@ -1,3 +1,4 @@
+% Uporabnik graficno izbere dve tocki v koordinatnem sistemu.
 % Program doloci koeficiente kubicnega polinoma skozi izhodisce,
 % ki minimizira cas potovanja kroglice, in ga narise.
 % Nato doloci polinom skozi tocke T1, T2, T3 iz podatkov.
@@ -8,10 +9,11 @@
 
 % Graficni vnos tock T1 in T2 na krivulji:
 [x,y] = ginput(2);
-x1 = x(1);
-y1 = y(1);
-x2 = x(2);
-y2 = y(2);
+% koordinate pomnozimo z 10, saj ginput izbira na [0,1]
+x1 = x(1)*10; 
+y1 = y(1)*10;
+x2 = x(2)*10;
+y2 = y(2)*10;
 % x1 = 1;
 % y1 = 5;
 % x2 = 7;
@@ -21,8 +23,7 @@ y2 = y(2);
 g = 9.8;
 
 % Dolocimo parameter a.
-a = doloci_polinom(x1,y1,x2,y2); % = -0.0440
-% a = -0.05; % numericni priblizek s poskusanjem
+a = doloci_polinom(x1,y1,x2,y2);
 
 % Izracunamo ostale koeficiente polinoma (skozi izhodisce) preko a.
 b = @(a) -3/2*a*(x2-x1);
@@ -37,9 +38,17 @@ r = @(x) a.*(x-x1).^3+b(a).*(x-x1).^2+c(a).*(x-x1)+d+y1;
 r(x1); % preverimo, da so vrednosti res y1 oz. y2
 r(x2);
 
-% Narisemo oba polinoma, na drugo sliko pa iskani polinom in brahistohrono:
+% SLIKE
+% Na prvi sliki sta polinoma skozi izhodisce in polinom skozi T1, T2, T3, 
+% ter oznacene tocke, ki polinoma dolocajo.
 hold on
 t = linspace(0,x2,100);
+plot(0, 0, 'x', 'color','k')
+plot(x2-x1, y2-y1, 'x', 'color','k')
+plot((x2-x1)/2, (y2-y1)/2, 'x', 'color','k')
+plot(x1, y1, 'x', 'color','k')
+plot(x2, y2, 'x', 'color','k')
+plot((x1+x2)/2, (y1+y2)/2, 'x', 'color','k')
 plot(t,p(t), '--')
 plot(t,r(t))
 grid on
@@ -47,14 +56,19 @@ hold off
 
 figure
 
+% Na drugi sliki sta iskan polinom, brahistohorna in tocke T1, T2, T3.
 hold on
 s = linspace(x1,x2,100);
+plot(x1, y1, 'x', 'color','k')
+plot(x2, y2, 'x', 'color','k')
+plot((x1+x2)/2, (y1+y2)/2, 'x', 'color','k')
 plot(s,r(s), 'r')
 risi_brahi([x1 y1],[x2 y2])
 hold off
 
+% CAS POTOVANJA
 % Cas potovanja izracunamo za polinom p (saj je v celoti pod x-osjo),
-% sicer pa sta casa potovanja enaka.
+% sicer pa sta casa potovanja po obeh polinomih enaka.
 dp = @(x) 3*a.*x.^2+2*b(a).*x+c(a);
 f = @(x) sqrt((1+dp(x).^2)./(-2*g.*p(x)));
 t_polinom = integral(f,0,x2-x1)
